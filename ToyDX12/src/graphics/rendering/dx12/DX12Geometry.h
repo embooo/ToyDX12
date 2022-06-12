2353b170f4145e5552ed136b32d68c27c9e08ff0
embooo <79663490+embooo@users.noreply.github.com>
2022-06-12 16:49:02 +0200

    Add temporary class to represent a mesh

---
+++ ToyDX12/src/graphics/rendering/dx12/DX12Geometry.h
@@ -0,0 +1,36 @@
+#include "pch.h"
+
+#include "DirectXMath.h"
+
+struct BasicVertex
+{
+	DirectX::XMFLOAT3 Pos;			// Offset : 0 bytes
+	DirectX::XMFLOAT2 TexCoord0;	// Offset : 12 bytes
+};
+
+const D3D12_INPUT_ELEMENT_DESC BasicVertexElementsDesc[]
+{
+	// https://docs.microsoft.com/en-us/windows/win32/api/d3d12/ns-d3d12-d3d12_input_element_desc
+	{ "POSITION",	0,	DXGI_FORMAT_R32G32B32_FLOAT,	0,	0,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+	{ "TEXCOORD",	0,	DXGI_FORMAT_R32G32_FLOAT,		0,	12,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}
+};
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 Pos;			// Offset : 0 bytes
+	DirectX::XMFLOAT3 Normal;		// Offset : 12 bytes
+	DirectX::XMFLOAT2 TexCoord0;	// Offset : 24 bytes
+	DirectX::XMFLOAT2 TexCoord1;	// Offset : 32 bytes
+};
+
+const D3D12_INPUT_ELEMENT_DESC VertexElementsDesc[]
+{
+	// https://docs.microsoft.com/en-us/windows/win32/api/d3d12/ns-d3d12-d3d12_input_element_desc
+	{ "POSITION",	0,	DXGI_FORMAT_R32G32B32_FLOAT,	0,	0,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+	{ "NORMAL",		0,	DXGI_FORMAT_R32G32B32_FLOAT,	0,	12,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+	{ "TEXCOORD",	0,	DXGI_FORMAT_R32G32_FLOAT,		0,	24,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+	{ "TEXCOORD",	1,	DXGI_FORMAT_R32G32_FLOAT,		0,	32,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+};
+
+static const D3D12_INPUT_LAYOUT_DESC BasicVertexInputLayoutDesc = { BasicVertexElementsDesc, 2 };
+static const D3D12_INPUT_LAYOUT_DESC VertexInputLayoutDesc		= { VertexElementsDesc, 4 };
