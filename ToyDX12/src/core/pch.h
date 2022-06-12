b7f71c693e37469e3a5c97c44d0fae210ec3ce5f
embooo <79663490+embooo@users.noreply.github.com>
2022-06-12 16:50:13 +0200

    Update pch.h

--- ToyDX12/src/core/pch.h
+++ ToyDX12/src/core/pch.h
@@ -4,6 +4,8 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
 #endif
 
+#include <numbers>
+
 #include <windows.h>
 #include <windowsx.h>
 #include <wrl.h>
