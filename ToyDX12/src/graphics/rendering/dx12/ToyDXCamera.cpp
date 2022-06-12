570339ec65b9f97c158f0da5816ad2b7e963575a
embooo <79663490+embooo@users.noreply.github.com>
2022-06-12 16:46:46 +0200

    Create Camera class

---
+++ ToyDX12/src/graphics/rendering/dx12/ToyDXCamera.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include "ToyDXCamera.h"
+
+using namespace DirectX;
+
+void ToyDX::Camera::Init()
+{
+	SetControlParameters();
+	SetFrustum();
+	BuildViewMatrix();
+}
+
+void ToyDX::Camera::Rotate(float fMousePosX, float fMousePosY)
+{
+	// Compute angle deltas in radians
+	float verticalAngleDeltaRad = m_fVerticalSensitivity * XMConvertToRadians(fMousePosX - m_fLastMousePosX);
+	float horizontalAngleDeltaRad = m_fHorizontalSensitivity * XMConvertToRadians(fMousePosY - m_fLastMousePosY);
+	
+	// Update angles
+	m_fTheta    = std::clamp(m_fTheta + verticalAngleDeltaRad, 0.1f, std::numbers::pi_v<float> -0.1f);
+	m_fPhi += horizontalAngleDeltaRad;
+
+	// Update previous mouse position
+	m_fLastMousePosX = fMousePosX;
+	m_fLastMousePosY = fMousePosY;
+}
+
+ToyDX::Camera& ToyDX::Camera::SetControlParameters(ControlParams st_ControlParams, float fRadius)
+{
+	m_stControlParams = st_ControlParams;
+	m_fRadius = fRadius;
+	return *this;
+}
+
+ToyDX::Camera& ToyDX::Camera::BuildViewMatrix()
+{
+	// Convert spherical (R, Theta, Phi) coordinates to cartesian (x, y, z)
+	float x = m_fRadius * cosf(m_fTheta) * sinf(m_fPhi);
+	float y = m_fRadius * sinf(m_fTheta) * sinf(m_fPhi);
+	float z = m_fRadius * cosf(m_fPhi);
+
+	XMVECTOR pos    = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up     = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX view = XMMatrixLookAtLH(pos, target, up);
+
+	return *this;
+}
+
+ToyDX::Camera& ToyDX::Camera::SetFrustum(Frustum st_Frustum)
+{
+	m_stFrustum = st_Frustum;
+	return *this;
+}
---
+++ ToyDX12/src/graphics/rendering/dx12/ToyDXCamera.h
@@ -0,0 +1,53 @@
+#pragma once
+
+namespace ToyDX
+{
+	struct Frustum
+	{
+		float fFovY = std::numbers::pi_v<float> / 3;
+		float fNear = 1.0f;
+		float fFar  = 1000.0f;
+	};
+
+	struct ControlParams
+	{
+		float fVerticalSensitivity   = 0.25f;
+		float fHorizontalSensitivity = 0.25f;
+	};
+
+	class Camera
+	{
+	public:
+		Camera() = default;
+
+		void Init();
+		void Rotate(float fMousePosX, float fMousePosY);
+		const Frustum& GetFrustum() { return m_stFrustum; }
+		Camera& SetControlParameters(ControlParams st_ControlParams = {}, float fRadius = 5.0);
+		Camera& SetFrustum(Frustum st_Frustum = {});
+		Camera& BuildViewMatrix();
+
+		DirectX::XMMATRIX& GetViewMatrix() { return m_ViewMatrix; }
+
+		~Camera() = default;
+	protected:
+		DirectX::XMMATRIX m_ViewMatrix;
+
+		DirectX::XMVECTOR m_Pos;
+		DirectX::XMVECTOR m_Target;
+		DirectX::XMVECTOR m_Up;
+
+		float m_fLastMousePosX = 0.0f;
+		float m_fLastMousePosY = 0.0f;
+		
+		float m_fVerticalSensitivity = 0.25f;
+		float m_fHorizontalSensitivity = 0.25f;
+
+		float m_fRadius = 5.0f;	
+		float m_fTheta = std::numbers::pi_v<float> / 4;	// Vertical angle
+		float m_fPhi   = 1.5f * std::numbers::pi_v<float>;	// Horizontal angle
+
+		Frustum m_stFrustum;
+		ControlParams m_stControlParams;
+	};
+}
