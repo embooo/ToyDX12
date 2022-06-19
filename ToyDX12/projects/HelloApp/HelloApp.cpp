9c0c2aea5db16b657ff88530d67e36d45cb9c363
embooo <79663490+embooo@users.noreply.github.com>
2022-06-19 20:48:29 +0200

    App drawing an indexed mesh, with camera, shader, PSO set

--- ToyDX12/projects/HelloApp/HelloApp.cpp
+++ ToyDX12/projects/HelloApp/HelloApp.cpp
@@ -2,11 +2,25 @@
 
 #include "HelloApp.h"
 
+#include "MathUtil.h"
+
+#include "TDXRenderer.h"
+
+#include "TDXMesh.h"
+
+using namespace DirectX;
+
 HelloApp::HelloApp(HINSTANCE hInstance, const char* sz_DebugName)
-	: DX12App(hInstance), m_DebugName(sz_DebugName)
+	: DX12App(hInstance), m_DebugName(sz_DebugName), m_DeltaTime(0.0)
 {
 
 }
+
+//*********************************************************
+
+
+
+//*********************************************************
 
 void HelloApp::Init()
 {
@@ -16,27 +30,47 @@ void HelloApp::Init()
 		return;
 	}
 
+	// Camera initialization
+	m_Camera.Init({ .fAspectRatio = DX12App::GetWindowAspectRatio(), .fFovY = DirectX::XMConvertToRadians(45.0f), .fNearZ = 1.0f,.fFarZ = 1000.0f,});
+	
+	// Renderer
+	m_Renderer = std::make_unique<ToyDX::Renderer>();
+	m_Renderer->Initialize();
+	
 	LOG_INFO("{0}::Init() Hello !", m_DebugName);
 }
 
-void HelloApp::Update()
+//*********************************************************
+
+void HelloApp::Update(double deltaTime)
 {
+	m_DeltaTime = deltaTime;
+	UpdateCamera(m_DeltaTime);
+
+	// Update constant buffer storing WVP matrix
+	PerObjectData constantsBufferData;
+	DirectX::XMStoreFloat4x4(&constantsBufferData.gWorld, XMMatrixTranspose(m_Renderer->m_Mesh->GetWorldMatrix()));
+	DirectX::XMStoreFloat4x4(&constantsBufferData.gView,  XMMatrixTranspose(m_Camera.GetViewMatrix()));
+	DirectX::XMStoreFloat4x4(&constantsBufferData.gProj,  XMMatrixTranspose(m_Camera.GetProjMatrix()));
+	m_Renderer->GetConstantBuffer()->CopyData(0, &constantsBufferData, sizeof(PerObjectData));
 }
 
-void HelloApp::Draw()
+//*********************************************************
+
+void HelloApp::Draw(double deltaTime)
 {
 	ID3D12CommandAllocator& rst_CommandAllocator = DX12RenderingPipeline::GetCommandAllocator();
-	ID3D12GraphicsCommandList& rst_CommandList   = DX12RenderingPipeline::GetCommandList();
-	ID3D12CommandQueue& rst_CommandQueue		 = DX12RenderingPipeline::GetCommandQueue();
+	ID3D12GraphicsCommandList& rst_CommandList = DX12RenderingPipeline::GetCommandList();
+	ID3D12CommandQueue& rst_CommandQueue = DX12RenderingPipeline::GetCommandQueue();
 
 	// Reuse the memory associated with command recording.
 	// We can only reset when the associated command lists have finished
 	// execution on the GPU.
 	ThrowIfFailed(rst_CommandAllocator.Reset());
-
+	
 	// A command list can be reset after it has been added to the
 	// command queue via ExecuteCommandList. Reusing the command list reuses memory.
-	ThrowIfFailed(rst_CommandList.Reset(&rst_CommandAllocator, nullptr));
+	ThrowIfFailed(rst_CommandList.Reset(&rst_CommandAllocator, m_Renderer->GetPipelineState()));
 
 	// Indicate a state transition on the resource usage.
 	ID3D12Resource* backBufferResource = mp_DX12RenderingPipeline->GetCurrentBackBuffer();
@@ -45,7 +79,6 @@ void HelloApp::Draw()
 
 	CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(backBufferResource, D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
 	rst_CommandList.ResourceBarrier(1, &barrier);
-
 
 	// Set the viewport and scissor rect. This needs to be reset
 	// whenever the command list is reset.
@@ -65,6 +98,11 @@ void HelloApp::Draw()
 	// Specify the buffers we are going to render to.
 	rst_CommandList.OMSetRenderTargets(1, &backBufferView, true, &depthStencilView);
 
+	{
+		m_Renderer->BindResources();
+		m_Renderer->Render();
+	}
+
 	// Indicate a state transition on the resource usage.
 	barrier = CD3DX12_RESOURCE_BARRIER::Transition(backBufferResource, D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
 	rst_CommandList.ResourceBarrier(1, &barrier);
@@ -83,11 +121,92 @@ void HelloApp::Draw()
 
 	// Wait until frame commands are complete.
 	DX12RenderingPipeline::FlushCommandQueue();
+
 }
+
+//*********************************************************
+
+void HelloApp::OnResize()
+{
+	m_Camera.UpdateProjMatrix();
+}
+
+void HelloApp::OnMouseMove(WPARAM buttonState, int xPos, int yPos)
+{
+	if (buttonState & MK_LBUTTON)
+	{
+		m_Camera.Rotate(xPos, yPos, m_DeltaTime);
+		m_Camera.UpdateViewMatrix();
+	}
+
+}
+
+void HelloApp::OnKeyPressed(WPARAM key, LPARAM lParam)
+{
+	//switch (key)
+	//{
+	//case VK_UP:
+	//	m_Camera.AddMoveState(CameraState::MoveForward);
+	//	break;
+	//case VK_DOWN:
+	//	m_Camera.AddMoveState(CameraState::MoveBackward);
+	//	break;
+	//case VK_LEFT:
+	//	m_Camera.AddMoveState(CameraState::MoveLeft);
+	//	break;
+	//case VK_RIGHT:
+	//	m_Camera.AddMoveState(CameraState::MoveRight);
+	//	break;
+	//}
+}
+
+void HelloApp::UpdateCamera(double deltaTime)
+{
+	if (GetAsyncKeyState('Z'))
+	{
+		m_Camera.AddMoveState(CameraState::MoveForward);
+	}
+
+	if (GetAsyncKeyState('S'))
+	{
+		m_Camera.AddMoveState(CameraState::MoveBackward);
+	}
+
+	if (GetAsyncKeyState('Q'))
+	{
+		m_Camera.AddMoveState(CameraState::MoveLeft);
+	}
+
+	if (GetAsyncKeyState('D'))
+	{
+		m_Camera.AddMoveState(CameraState::MoveRight);
+	}
+
+	if (GetAsyncKeyState(VK_SPACE))
+	{
+		m_Camera.AddMoveState(CameraState::MoveUp);
+	}
+
+	if ( GetAsyncKeyState(VK_LSHIFT))
+	{
+		m_Camera.AddMoveState(CameraState::MoveDown);
+	}
+
+	if (m_Camera.NeedUpdate())
+	{
+		m_Camera.UpdatePosition(deltaTime);
+		m_Camera.UpdateViewMatrix();
+	}
+}
+
+//*********************************************************
+//*********************************************************
 
 void HelloApp::Terminate()
 {
 }
+
+//*********************************************************
 
 HelloApp::~HelloApp()
 {
--- ToyDX12/projects/HelloApp/HelloApp.h
+++ ToyDX12/projects/HelloApp/HelloApp.h
@@ -1,17 +1,48 @@
 #pragma once
 
 #include "DX12App.h"
+#include "MathUtil.h"
+
+#include "ToyDXCamera.h"
+
+namespace ToyDX
+{
+	class UploadBuffer;
+	class Renderer;
+}
 
 class HelloApp : public DX12App
 {
 public:
-	HelloApp(HINSTANCE hInstance, const char* sz_DebugName = "DefaultAppName");
+	HelloApp(HINSTANCE hInstance, const char* sz_DebugName = "HelloApp");
 	~HelloApp() override;
 
 	virtual void Init() override;
-	virtual void Update() override;
-	virtual void Draw() override;
+	virtual void Update(double deltaTime) override;
+	virtual void Draw(double deltaTime) override;
+
 	virtual void Terminate() override;
+
+	// Event handlers
+	virtual void OnResize() override;
+	virtual void OnMouseMove(WPARAM buttonState, int xPos, int yPos) override;
+	virtual void OnKeyPressed(WPARAM buttonState, LPARAM lParam) override;
+
+	// 
+	void UpdateCamera(double deltaTime);
+	double m_DeltaTime;
+
 protected:
 	const char* m_DebugName;
+	
+	// Upload buffers
+	ComPtr<ID3D12Resource> p_VertexBufferCPU = nullptr; 
+	ComPtr<ID3D12Resource> p_IndexBufferCPU  = nullptr; 
+
+	// Heaps
+	ComPtr<ID3D12DescriptorHeap> m_CbvHeap;
+
+protected:
+	ToyDX::Camera m_Camera;
+	std::unique_ptr<ToyDX::Renderer> m_Renderer;
 };
