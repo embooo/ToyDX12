ad68b673d1f3f36d38a92d562d82ad556f44623d
embooo <79663490+embooo@users.noreply.github.com>
2022-07-03 18:13:03 +0200

    Create Drawable class
    Describes a drawable object, with its parameters

---
+++ ToyDX12/src/graphics/rendering/dx12/Drawable.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+#include "Drawable.h"
+
+namespace ToyDX
+{
+	Drawable::Drawable(ToyDX::Mesh* mesh)
+		: Mesh(mesh)
+	{
+		// Check if the mesh is an aggregate of primitives, with different offsets in the vertex/index buffer
+		// or a single primitive, spanning the whole buffers
+
+		HasSubMeshes = mesh->Data.Primitives.size() > 1;
+
+		if (HasSubMeshes == false)
+		{
+			NumIndices = mesh->Data.Primitives[0].NumIndices;
+			StartIndexLocation = mesh->Data.Primitives[0].StartIndexLocation;
+			BaseVertexLocation = mesh->Data.Primitives[0].BaseVertexLocation;
+		}
+	}
+}
+
+
+
---
+++ ToyDX12/src/graphics/rendering/dx12/Drawable.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "DirectXMath.h"
+#include "MathUtil.h"
+#include "d3d12.h"
+#include "TDXMesh.h"
+
+static const int DefaultNumFrameResources = 3;
+
+namespace ToyDX
+{
+	// Describes a drawable item and the data that is needed to render it
+	class Drawable
+	{
+	public:
+		Drawable() = default;
+		Drawable(Mesh* mesh);
+
+		DirectX::XMMATRIX WorldMatrix = DirectX::XMMatrixTranslationFromVector({0,0,0});
+
+		Mesh* Mesh = nullptr;
+		
+		bool HasSubMeshes = false;
+
+		// Number of frame resources that need an update, initially : all frame resources
+		unsigned int NumFramesDirty = DefaultNumFrameResources;
+
+		// Index in GPU constant buffer of the corresponding per object CB for this drawable
+		size_t PerObjectCbIndex = -1;
+
+		size_t NumIndices = 0;	
+		size_t StartIndexLocation = 0;	// The location of the first index read by the GPU from the index buffer. == Number of indices before the first index of this primitive
+		size_t BaseVertexLocation = 0;  // A value added to each index before reading a vertex from the vertex buffer == Number of vertices before the first vertex of this primitive
+
+		// Type of geometric primitive
+		D3D12_PRIMITIVE_TOPOLOGY PrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	};
+}
