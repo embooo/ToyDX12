9c73dad44cc63a9bad57aaffd94505b0680d6b07
embooo <79663490+embooo@users.noreply.github.com>
2022-07-03 18:19:08 +0200

    Use constant buffers (per pass , per object)

--- ToyDX12/projects/HelloApp/data/shaders/DefaultPixel.hlsl
+++ ToyDX12/projects/HelloApp/data/shaders/DefaultPixel.hlsl
@@ -3,9 +3,11 @@ struct VSOutput
     float4 PosCS : SV_POSITION;
     float4 PosWS : POSITION;
     float3 NormalWS : NORMAL;
+    float3 Tangent : TANGENT;
+    float2 TexCoord : TEXCOORD;
 };
 
 float4 main(VSOutput psInput) : SV_TARGET
 {
-    return float4(normalize(psInput.NormalWS.xyz), 1.0);
+    return float4(psInput.NormalWS.xyz, 1.0);
 }
--- ToyDX12/projects/HelloApp/data/shaders/DefaultVertex.hlsl
+++ ToyDX12/projects/HelloApp/data/shaders/DefaultVertex.hlsl
@@ -2,8 +2,21 @@
 cbuffer cbPerObject : register(b0)
 {
     float4x4 gWorld;
+};
+
+cbuffer cbPerPass : register(b1)
+{
+    // Matrices
     float4x4 gView;
+    float4x4 gInvView;
     float4x4 gProj;
+    float4x4 gInvProj;
+    float3   gEyePosWS;
+    float pad0;
+    float gNear;
+    float gFar;
+    float gDeltaTime;
+    float gTotalTime;
 };
 
 // Other syntax (since shader model 5.1)
@@ -16,9 +29,10 @@ cbuffer cbPerObject : register(b0)
 
 struct VSInput
 {
-    float3 Pos : POSITION;
+    float3 PosOS : POSITION;
     float3 NormalOS : NORMAL;
-
+    float3 Tangent : TANGENT;
+    float2 TexCoord : TEXCOORD;
 };
 
 struct VSOutput
@@ -26,6 +40,8 @@ struct VSOutput
     float4 PosCS : SV_POSITION;
     float4 PosWS : POSITION;
     float3 NormalWS : NORMAL;
+    float3 Tangent : TANGENT;
+    float2 TexCoord : TEXCOORD;
 };
 
 VSOutput main(VSInput input)
@@ -33,9 +49,12 @@ VSOutput main(VSInput input)
     float4x4 wvp = mul(mul(gWorld, gView), gProj);
     VSOutput output;
 
-    output.PosCS = mul(float4(input.Pos, 1.0), wvp);
-    output.NormalWS = mul(float4(input.NormalOS, 0.0), gWorld);
-    output.PosWS =  mul(float4(input.Pos, 1.0), gWorld);
+    output.PosCS = mul(float4(input.PosOS, 1.0), wvp);
+    output.PosCS = output.PosCS;
+    output.NormalWS = mul(float4(input.NormalOS, 0.0), gWorld).xyz;
+    output.PosWS =  mul(float4(input.PosOS, 1.0), gWorld);
+    output.Tangent = input.Tangent;
+    output.TexCoord = input.TexCoord;
 
     return output;
 }
