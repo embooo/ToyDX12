312805b698bf023c1d0123af655ac981a9b97906
embooo <79663490+embooo@users.noreply.github.com>
2022-07-03 18:17:20 +0200

    Load GLTF mesh with transforms

--- ToyDX12/src/graphics/core/MeshLoader.cpp
+++ ToyDX12/src/graphics/core/MeshLoader.cpp
@@ -3,9 +3,11 @@
 
 #include "Logger.h"
 #include "MeshLoader.h"
+#include "TDXMesh.h"
 #include "DirectXMath.h"
 
-void MeshLoader::LoadGltf(const char* sz_Filename, GltfMesh& mesh)
+
+void MeshLoader::LoadGltf(const char* sz_Filename, MeshData* mesh)
 {
 	cgltf_options options = { };
 	cgltf_data* data = NULL;
@@ -20,37 +22,31 @@ void MeshLoader::LoadGltf(const char* sz_Filename, GltfMesh& mesh)
 			for (size_t i = 0; i < data->nodes_count; ++i)
 			{
 				cgltf_node& currNode = data->nodes[i];
-				LOG_DEBUG("=====================================");
-				LOG_DEBUG("Node : {0}, Children : {1}", currNode.name ? currNode.name : "UNNAMED", currNode.children_count);
 
 				ProcessGltfNode(false, &currNode, mesh);
-				
-				for (size_t j = 0; j < currNode.children_count; ++j)
-				{
-					LOG_DEBUG("  Child Node : {0}, Children : {1}", currNode.name ? currNode.name : "Unnamed", currNode.children_count);
-					ProcessGltfNode(true, currNode.children[j], mesh);
-				}
-
 			}
 		}
+
+		mesh->WholeMesh = {.NumIndices = mesh->Vertices.size(), .StartIndexLocation = 0, .BaseVertexLocation = 0};
 
 		cgltf_free(data);
 	}
 	else
 	{
 		LOG_ERROR("Could not read GLTF/GLB file. [error code : {0}]", result);
-		return;
+		assert(false);
 	}
 }
 
-static void LoadVertices(cgltf_primitive* primitive, GltfMesh& st_Mesh)
+static void LoadVertices(cgltf_primitive* primitive, MeshData* st_Mesh, const DirectX::XMMATRIX& currWorldMat)
 {
-	size_t firstVertex = st_Mesh.vertices.size();
+	size_t firstVertex = st_Mesh->Vertices.size();
 
 	std::vector<DirectX::XMFLOAT3> positionsBuffer;
 	std::vector<DirectX::XMFLOAT3> normalsBuffer;
 	std::vector<DirectX::XMFLOAT2> texCoordBuffer;
-
+	std::vector<DirectX::XMFLOAT3> tangentBuffer;
+	
 	// Build raw buffers containing each attributes components
 	for (int attribIdx = 0; attribIdx < primitive->attributes_count; ++attribIdx)
 	{
@@ -87,6 +83,15 @@ static void LoadVertices(cgltf_primitive* primitive, GltfMesh& st_Mesh)
 				cgltf_accessor_read_float(attribute->data, i, &texCoordBuffer[i].x, 2);
 			}
 		}
+		case cgltf_attribute_type_tangent:
+		{
+			//LOG_DEBUG("        Tangents : {0}", attribute->data->count);
+			tangentBuffer.resize(attribute->data->count);
+			for (int i = 0; i < tangentBuffer.size(); ++i)
+			{
+				cgltf_accessor_read_float(attribute->data, i, &tangentBuffer[i].x, 3);
+			}
+		}
 		break;
 
 		}
@@ -95,64 +100,95 @@ static void LoadVertices(cgltf_primitive* primitive, GltfMesh& st_Mesh)
 	// Build vertices
 	for (int i = 0; i < positionsBuffer.size(); ++i)
 	{
-		Vertex vertex =
+		auto Pos = DirectX::XMVector3Transform(DirectX::XMLoadFloat3(&positionsBuffer[i]), currWorldMat);
+		
+		Vertex vertex;
 		{
-			.Pos    = positionsBuffer[i], 
-			.Normal = normalsBuffer[i],
-			.TexCoord0 = texCoordBuffer[i],
-			.TexCoord1 = {0,0}
-		};
+			DirectX::XMStoreFloat3(&vertex.Pos, Pos);
+			vertex.Normal = normalsBuffer[i],
+			vertex.Tangent = tangentBuffer[i];
+			vertex.TexCoord0 = texCoordBuffer[i];
+		}
 
-		st_Mesh.vertices.push_back(vertex);
+		st_Mesh->Vertices.push_back(vertex);
 	}
-
+	
 	//LOG_ERROR("        Vertex buffer size : {0}", st_Mesh.vertices.size());
-
-
 }
 
-static void LoadIndices(cgltf_primitive* primitive, GltfMesh& st_Mesh)
+static void LoadIndices(cgltf_primitive* primitive, MeshData* st_Mesh)
 {
-	st_Mesh.primitives.back().NumIndices = primitive->indices->count;
+	st_Mesh->Primitives.back().NumIndices = primitive->indices->count;
 	for (int idx = 0; idx < primitive->indices->count; ++idx)
 	{
-		st_Mesh.indices.push_back(uint16_t(cgltf_accessor_read_index(primitive->indices, idx)));
+		st_Mesh->Indices.push_back(uint16_t(cgltf_accessor_read_index(primitive->indices, idx)));
 	}
 
 	//LOG_ERROR("        Index buffer size : {0}", st_Mesh.indices.size());
 }
 
-static void LoadPrimitive(cgltf_primitive* primitive, GltfMesh& st_Mesh)
+static void LoadPrimitive(cgltf_primitive* primitive, MeshData* st_Mesh, const DirectX::XMMATRIX& currWorldMat)
 {
 	//LOG_DEBUG("      Primitive : {0} attributes, {1} indices", primitive->attributes_count, primitive->indices->count);
 
-	GltfPrimitive p = {.StartIndexLocation = st_Mesh.indices.size(), .BaseVertexLocation = st_Mesh.vertices.size()};
-	st_Mesh.primitives.push_back(p);
+	Primitive p = {.StartIndexLocation = st_Mesh->Indices.size(), .BaseVertexLocation = st_Mesh->Vertices.size()};
+	st_Mesh->Primitives.push_back(p);
 
 	LoadIndices(primitive, st_Mesh);
-	LoadVertices(primitive, st_Mesh);
+	LoadVertices(primitive, st_Mesh, currWorldMat);
 }
 
 
-static void LoadMesh(cgltf_mesh* mesh, GltfMesh& st_Mesh)
+static void LoadMesh(cgltf_mesh* mesh, MeshData* st_Mesh, const DirectX::XMMATRIX& currWorldMat)
 {
 	LOG_DEBUG("    Mesh '{0}': {1} primitives", mesh->name ? mesh->name : "Unnamed", mesh->primitives_count);
 
 	for (int i = 0; i < mesh->primitives_count; ++i)
 	{
-		LoadPrimitive(&mesh->primitives[i], st_Mesh);
+		LoadPrimitive(&mesh->primitives[i], st_Mesh, currWorldMat);
 	}
 }
 
-void MeshLoader::ProcessGltfNode(bool bIsChild, cgltf_node* p_Node, GltfMesh& mesh)
+void LoadTransform(cgltf_node* p_Node, DirectX::XMMATRIX& currWorldMat)
 {
+	LOG_DEBUG("TRANSFORM : World");
+	float worldMat[16];
+	cgltf_node_transform_world(p_Node, worldMat);
+
+	currWorldMat = DirectX::XMMATRIX(worldMat);
+	for (int i = 0; i < 16; i += 4)
+	{
+		LOG_DEBUG("{0}, {1}, {2}, {3}", worldMat[i], worldMat[i + 1], worldMat[i + 2], worldMat[i + 3]);
+	}
+}
+
+void MeshLoader::ProcessGltfNode(bool bIsChild, cgltf_node* p_Node, MeshData* mesh)
+{
+	DirectX::XMMATRIX currWorldMat = DirectX::XMMatrixIdentity();
+
+	if (bIsChild)
+	{
+		LOG_DEBUG("    Child Node : {0}, Children : {1}", p_Node->name ? p_Node->name : "Unnamed", p_Node->children_count);
+	}
+	else
+	{
+		LOG_DEBUG("=====================================");
+		LOG_DEBUG("Node : {0}, Children : {1}", p_Node->name ? p_Node->name : "Unnamed", p_Node->children_count);
+	}
+
 	if (p_Node->mesh)
 	{
-		float WorldTransformBuffer[16];
-		cgltf_node_transform_world(p_Node, WorldTransformBuffer);
-		DirectX::XMMATRIX WorldTransformMat(WorldTransformBuffer);
-		mesh.worldMatrix = WorldTransformMat;
-		LoadMesh(p_Node->mesh, mesh);
+		LoadTransform(p_Node, currWorldMat);
+		LoadMesh(p_Node->mesh, mesh, currWorldMat);
+	}
+
+	for (size_t j = 0; j < p_Node->children_count; ++j)
+	{
+		if (j == 0)
+		{
+			LOG_DEBUG("  Parent Node : {0}", p_Node->name ? p_Node->name : "Unnamed");
+		}
+		ProcessGltfNode(true, p_Node->children[j], mesh);
 	}
 }
 
--- ToyDX12/src/graphics/core/MeshLoader.h
+++ ToyDX12/src/graphics/core/MeshLoader.h
@@ -3,32 +3,7 @@
 #include "DX12Geometry.h"
 
 class cgltf_node;
-
-struct GltfMesh;
-struct GltfPrimitive;
-struct GltfModel;
-
-struct GltfMesh
-{
-	DirectX::XMMATRIX worldMatrix;
-	std::vector<Vertex>   vertices;
-	std::vector<uint16_t> indices;
-
-	std::vector<GltfPrimitive> primitives;
-};
-
-
-struct GltfPrimitive
-{
-	size_t NumIndices;	
-	size_t StartIndexLocation;	// The location of the first index read by the GPU from the index buffer. == Number of indices before the first index of this primitive
-	size_t BaseVertexLocation;  // A value added to each index before reading a vertex from the vertex buffer == Number of vertices before the first vertex of this primitive
-};
-
-struct GltfModel
-{
-	std::vector<GltfMesh> meshes;
-};
+struct MeshData;
 
 class MeshLoader
 {
@@ -36,8 +11,8 @@ public:
 	MeshLoader() = delete;
 	~MeshLoader() = delete;
 
-	static void LoadGltf(const char* sz_Filename, GltfMesh& mesh);
+	static void LoadGltf(const char* sz_Filename, MeshData* mesh);
 
 protected:
-	static void ProcessGltfNode(bool bIsChild, cgltf_node* p_Node, GltfMesh& mesh);
+	static void ProcessGltfNode(bool bIsChild, cgltf_node* p_Node, MeshData* mesh);
 };
